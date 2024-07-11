151e68d3d320e4a51e74c6ce2949aa6d14194be9
AllaArestakesyan <alla.arestakesyan@gmail.com>
2024-07-11 16:29:10 +0400

    first

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_ITEMS = gql`
+  query GetItems {
+    user{
+      id
+      name
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_ITEMS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error {':('}</p>;
+
   return (
-    <div className="App">
-    
+    <div>
+      <h1>Items</h1>
+      <ul>
+        {data.items.map((item:any, index:any) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
